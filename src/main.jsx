67634f186b3fe69f46515df7e0f3eed057cb78dd
Essuon Emmanuel <essuonemma84@gmail.com>
2024-02-16 14:25:14 +0000

    worked on react router navigation

--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,11 @@ import './index.css'
 
 import { createBrowserRouter, RouterProvider} from 'react-router-dom'
 import ErrorPage from './error-page'
-import Contact from './routes/contact'
+import Contact, {loader as contactLoader} from './routes/contact'
 import Root, { loader as rootLoader, action as rootAction } from "./routes/roots";
+import EditContact, {action as editAction} from './routes/edit'
+import { action as destroyAction } from './routes/destroy'
+import Index from './routes'
 
 const router = createBrowserRouter([
   {
@@ -14,10 +17,27 @@ const router = createBrowserRouter([
     errorElement: <ErrorPage />,
     loader: rootLoader,
     action: rootAction,
-    children:[{   
+    children:[
+      {index: true, 
+      element: <Index />
+      }
+      ,{   
       path: "contacts/:contactId",
       element: <Contact />,
-    }],
+      loader: contactLoader,
+    },
+    {
+     path: "contacts/:contactId/edit",
+     element: <EditContact />,
+     loader: contactLoader,
+     action: editAction,
+    },
+    {
+      path: "contacts/:contactId/destroy",
+      action: destroyAction,
+      errorElement: <div>Oops! There was an error.</div>,
+    },
+  ],
   },
   ],
   )
